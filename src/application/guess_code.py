18fe3f7fb0eb1b44694c93e6fe275b87a63d2c04
Yeray Diaz <yeray.diaz@universia.net>
2022-11-28 02:29:36 +0100

    fix: Include last guess

--- src/application/guess_code.py
+++ src/application/guess_code.py
@@ -16,7 +16,7 @@ class GuessCodeHandler:
 
     def handle(self, request: GuessCodeRequest) -> Game:
         for code_value in request.guess:
-            if not code_value in [item.value for item in GameColor]:
+            if code_value not in [item.value for item in GameColor]:
                 raise InvalidGuessValuesException()
 
         with self.games_uow:
@@ -33,25 +33,27 @@ class GuessCodeHandler:
             self.games_uow.games.add_guess(guess)
             self.games_uow.games.update(game)
             self.games_uow.commit()
-            game_copy = Game(
-                identifier=game.identifier,
-                code=game.code,
-                max_tries=game.max_tries,
-            )
-            game_copy.tries = game.tries
-            game_copy.date_created = game.date_created
-            game_copy.date_modified = game.date_modified
-            game_copy.guesses = [
-                Guess(
-                    identifier=guess.identifier,
-                    game_id=guess.game_id,
-                    code=guess.code,
-                    black_peqs=guess.black_peqs,
-                    white_peqs=guess.white_peqs,
-                    date_created=guess.date_created
-                )
-                for guess in game.guesses
-            ]
-            game_copy.guessed = game.guessed
+            return self.__copy_game(game, guess)
 
+    def __copy_game(self, game: Game, guess: Guess):
+        game_copy = Game(
+            identifier=game.identifier,
+            code=game.code,
+            max_tries=game.max_tries,
+        )
+        game_copy.tries = game.tries
+        game_copy.date_created = game.date_created
+        game_copy.date_modified = game.date_modified
+        game_copy.guesses = [
+            Guess(
+                identifier=guess.identifier,
+                game_id=guess.game_id,
+                code=guess.code,
+                black_peqs=guess.black_peqs,
+                white_peqs=guess.white_peqs,
+                date_created=guess.date_created
+            )
+            for guess in game.guesses + [guess]
+        ]
+        game_copy.guessed = game.guessed
         return game_copy
